ed531ccc4985d04078cbc6b5a87a2eb89e542af8
AtharvaKhedkar <atharva.khedkar123@gmail.com>
2024-06-10 02:26:03 -0700

    add noisecancelling application

---
+++ mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/noisecancelling.py
@@ -0,0 +1,29 @@
+def main() {
+var fs = 8000;
+  # var step = 1/8000; 
+  # print(step);
+  var t = getRangeOfVector(0,100, 0.000125);
+  var f_sig = 500;
+  var pi = 3.14159265359;
+  var getMultiplier = 2 * pi * f_sig;
+  # print(getMultiplier);
+  var getSinDuration = gain(t, getMultiplier);
+  # print(getSinDuration);
+  var clean_sig = sin(getSinDuration );
+
+  #define a noise signal with freq = 3000
+  var f_noise = 3000;
+  var getNoiseSinDuration = gain(t, 2 * pi * f_noise);
+  var noise = sin(getNoiseSinDuration);
+  var noise1 = gain(noise, 0.5);
+
+  var noisy_sig = clean_sig + noise1;
+  # print(noisy_sig);
+  # print(clean_sig);
+  var mu = 0.01;
+  var filterSize = 32;
+  var y = lmsFilterResponse(noisy_sig, clean_sig, mu, filterSize);
+  print(y);
+
+}
+
