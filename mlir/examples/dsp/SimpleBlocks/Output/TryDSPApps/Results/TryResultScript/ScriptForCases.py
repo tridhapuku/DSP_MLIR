f45bc9607ccdd06ecccd200999e24664c3c8b9df
tridhapuku <abhidipu863@gmail.com>
2024-07-03 20:29:58 -0700

    ScriptForCases.py: dump Output in Output directory, generate 3 diffnt executables for the 3 cases , ScriptForMlirAffine.py : dump both Mlir-Affine and llvm files in output dir

--- mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/Results/TryResultScript/ScriptForCases.py
+++ mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/Results/TryResultScript/ScriptForCases.py
@@ -3,22 +3,40 @@ import subprocess
 import time
 
 # The script does the following
-# Input : filename.py , enableAffineOpt , enableCanonOpt  
+# Input : filename.py 
 # Output : TimeOfExecution for different IP sizes : 
+# Steps to run:
+    # Open a terminal at the path of the script -- 
+    # Run: python ScriptForCases.py #3.11 validated 
+
 # Pseudo-code:
     # Iterate for all the input-size & update the input value in file
     # Update logic -- change the 2nd parameter of line: var c = getRangeOfVector(init , Count, StepSize)
     # Run the respective commands on the file
 
 # Path to the input file
-input_file_path = "back2backDelay.py"      # " audioCompression.py"
-# enableAffineOpt = False  # Set to True to enable affine optimization
-# enableCanonOpt = False   # Set to True to enable canonical optimization
+# Apps = "noisecancelling.py" , "lowPassFull.py" , " audioCompression.py" ,
+        #  "back2backDelay.py" , "lowPassFIRFilterDesign.py" ,
+input_file_path =  "noisecancelling.py" 
+BasePathForLLVM = "/mnt/sharedDrive/SourceCode/llvm-project/"
+OutputScriptPath = "mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/Results/TryResultScript/"
+# OutputPath = BasePathForLLVM + "mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/Results/TryResultScript/Output/"
 
+# Construct full output path
+OutputPath = os.path.join(BasePathForLLVM, OutputScriptPath, "Output")
+
+# Check if the Output folder exists, create it if it doesn't
+if not os.path.exists(OutputPath):
+    os.makedirs(OutputPath)
+
+# Now OutputPath is ready for use
+print("InputPath:{}".format(BasePathForLLVM))
+print(f"OutputPath: {OutputPath}")
+# exit()
 
 # ************ Don't change unless u required 
 # Define the values dictionary
-values = {
+inputValues = {
     # "10": 10,
     # "100": 100,
     # "1K": 1000,
@@ -29,75 +47,53 @@ values = {
     # "50K": 50000,
     # "100K": 100000,
      "1M": 1000000,
-    # "10M": 10000000,
-    # "20M": 20000000,
-    # "30M": 30000000,
-    # "40M": 40000000,
-    # "50M": 50000000,
-    # "100M": 100000000,
+    "10M": 10000000,
+    "20M": 20000000,
+    "30M": 30000000,
+    "40M": 40000000,
+    "50M": 50000000,
+    "100M": 100000000,
     # "1B": 1000000000
 }
-NoOfIterations = 100
+NoOfIterations = 3
 
-# OptCase = []
-
-# commands_base = [
-#     "./dsp1 lowPassFull.py -emit=llvm",
-#     "clang-17 file.ll -o fileexe -lm",
-#     # "./fileexe"
-# ]
-
+# --------------------------------------------------
 commands_base = [
     # "./dsp1 lowPassFull.py -emit=mlir-affine",
     f"./dsp1 {input_file_path} -emit=llvm",
+    # f"{BasePathForLLVM}build/bin/dsp1 {input_file_path} -emit=llvm",
     "clang-17 -O0 file.ll -o fileexe -lm",
-    # "./fileexe"
 ]
 
 # Define the cases
-# cases = [
-#     {"affineOpt": False, "canonOpt": False, "suffix": "file2.ll"},
-#     {"affineOpt": True, "canonOpt": False, "suffix": "file3.ll"},
-#     {"affineOpt": True, "canonOpt": True, "suffix": "file4.ll"},
-# ]
-
-cases = [
-    # {"affineOpt": True, "canonOpt": True, "suffix": "fileAffineCanonOpt.ll"},
-    # {"affineOpt": True, "canonOpt": False, "suffix": "fileAffineOpt.ll"},
-    {"affineOpt": False, "canonOpt": False, "suffix": "fileNoOpt.ll"},    
+cases = [  
+    {"affineOpt": False, "canonOpt": False, "suffix": "fileNoOpt.ll" , "exe" : "fileNoOptExe"}, 
+    {"affineOpt": True, "canonOpt": False, "suffix": "fileAffineOpt.ll" , "exe" : "fileAffineOptExe"},
+    {"affineOpt": True, "canonOpt": True, "suffix": "fileAffineCanonOpt.ll", "exe" : "fileAffineCanonOptExe"},   
 ]
-
-
-
-# Modify the base command based on flags
-# if enableAffineOpt and enableCanonOpt:
-#     commands_base[0] += " -affineOpt -canonOpt 2> file4.ll"
-#     commands_base[1] = "clang-17 file4.ll -o fileexe -lm"
-# elif enableAffineOpt:
-#     commands_base[0] += " -affineOpt 2> file3.ll"
-#     commands_base[1] = "clang-17 file3.ll -o fileexe -lm"
-# else:
-#     commands_base[0] += " 2> file2.ll"
-#     commands_base[1] = "clang-17 file2.ll -o fileexe -lm"
-
-
 
 # Read the input file
 with open(input_file_path, "r") as file:
     lines = file.readlines()
-   
-for key, value in values.items():
+
+print("",end="\t")
+for case in cases:
+    print(f"{case['exe']}",end="\t")
+
+# print("\n")   
+for key, value in inputValues.items():
     # Update the specific line in the file
     # print("Updating for {}".format(value))
-    print("\n")
-    print("{}".format(key), end="\t")
+    # print("\n")
+    print("\n{}".format(key), end="\t")
     with open(input_file_path, "w") as file:
         for line in lines:
             if line.strip().startswith("var input = getRangeOfVector("):
             # if line.strip().startswith("var N = "):
                 # Replace the second parameter with the current value
-                updated_line = f"    var input = getRangeOfVector(0, {value}, 10);\n"
-                # updated_line = f"    var N = {value} ;\n"
+                updated_line = f"\tvar input = getRangeOfVector(0, {value}, 0.000125);\n"
+                # updated_line = f"\tvar input = getRangeOfVector(0, {value}, 1);\n"
+                # updated_line = f"    var N = {value + 1} ;\n"
                 file.write(updated_line)
             else:
                 file.write(line)
@@ -110,16 +106,18 @@ for key, value in values.items():
             command_llvm += " -affineOpt"
         if case["canonOpt"]:
             command_llvm += " -canonOpt"
-        command_llvm += f" 2> {case['suffix']}"
+        # command_llvm += f" 2> {case['suffix']}" #OutputPath
+        command_llvm += f" 2> {OutputPath}/{case['suffix']}" #OutputPath
 
         commands = [
             command_llvm,
-            f"clang-17 {case['suffix']} -o fileexe -lm",
+            # f"clang-17 -O0 {case['suffix']} -o fileexe -lm",
+            f"clang-17 -O0 {OutputPath}/{case['suffix']} -o {OutputPath}/{case['exe']} -lm",
         ] 
         # print(case,end="\n")
         # print("\n")
-        # Iterate over each value and perform the necessary operations
         
+        # Iterate over each value and perform the necessary operations       
         for command in commands:
             # Run the commands for the current case
             result = subprocess.run(command, shell=True, capture_output=True, text=True)  
@@ -133,19 +131,29 @@ for key, value in values.items():
             
             # Clear the cache to minimize caching effects
             # subprocess.run("sync; echo 3 > /proc/sys/vm/drop_caches", shell=True)
-            subprocess.run("sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'", shell=True, check=True)
-
+            try:
+                process = subprocess.run("sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'", shell=True, check=True)
+                # process.wait()
+            except subprocess.CalledProcessError:
+                process.terminate()
             # The command to be executed
             # command2 = "./fileexe"
             # Limit execution to a single core
-            command2 = "taskset -c 0 ./fileexe"  
+            # command2 = "taskset -c 0 ./fileexe"  
+            # command2 = f"taskset -c 0 ./{case['exe']}" #{OutputPath}
+            command2 = f"taskset -c 0 ./Output/{case['exe']}"
 
             # Record the start time
             start_time = time.time()
 
             # Execute the command
-            subprocess.run(command2, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
-            # subprocess.run(command2, shell=True)
+            try:
+                subprocess.run(command2, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+                # subprocess.run(command2, shell=True)
+            except subprocess.CalledProcessError as exc:
+                print(f"Process failed because did not return a successful return code. "
+                        f"Returned {exc.returncode}\n{exc}")
+            
 
             # Record the end time
             end_time = time.time()
@@ -157,19 +165,3 @@ for key, value in values.items():
         avg_exe_time = sum_exe_time / NoOfIterations
         print("{}".format(avg_exe_time), end="\t")
     # print(f"The command took {execution_time} seconds to execute.")
-
-
-
-
-
-
-# Commands to run for each case
-    # commands = [
-    #     # "./dsp1 audioCompression.py -emit=llvm 2> file2.ll",
-    #     # "clang-17 file2.ll -o file2exe -lm",
-    #     # "(time ./file2exe > file123) 2> timeOut",
-    #     # "time ./file2exe "
-    #     "./dsp1 audioCompression.py -emit=llvm -affineOpt 2> file3.ll",
-    #     "clang-17 file3.ll -o file3exe -lm",
-
-    # ]
--- mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/Results/TryResultScript/ScriptForMlirAffine.py
+++ mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/Results/TryResultScript/ScriptForMlirAffine.py
@@ -3,23 +3,43 @@ import subprocess
 import time
 
 # The script does the following
-# Input : filename.py , enableAffineOpt , enableCanonOpt  
-# Output : TimeOfExecution for different IP sizes : 
+# Input : filename.py   
+# Output : Mlir-affine level IR & LLVM IR for 3 cases--NoOpt, AffineOpt , AffineCanonOpt : 
 # Pseudo-code:
     # Iterate for all the input-size & update the input value in file
-    # Update logic -- change the 2nd parameter of line: var c = getRangeOfVector(init , Count, StepSize)
-    # Run the respective commands on the file
+    # Update logic -- change the 2nd parameter of line: var input = getRangeOfVector(init , Count, StepSize)
+    # Run the respective commands on the file & generate 
+        # AffineLevelFiles:  fileNoOpt.mlir , fileAffineOpt.mlir & fileAffineCanonOpt.mlir
+        # LLVM Files:  fileNoOpt.ll , fileAffineOpt.ll & fileAffineCanonOpt.ll
+
+#To Do:
+    # CHeck for various failure cases
+
+# Apps = "noisecancelling.py" , "lowPassFull.py" , " audioCompression.py" ,
+        #  "back2backDelay.py" , "lowPassFIRFilterDesign.py" ,
 
 # Path to the input file
-input_file_path = "back2backDelay.py"   #"audioCompression.py"
-# enableAffineOpt = False  # Set to True to enable affine optimization
-# enableCanonOpt = False   # Set to True to enable canonical optimization
+input_file_path = "noisecancelling.py"  
+BasePathForLLVM = "/mnt/sharedDrive/SourceCode/llvm-project/"
+OutputScriptPath = "mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/Results/TryResultScript/"
+# OutputPath = BasePathForLLVM + "mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/Results/TryResultScript/Output/"
 
+# Construct full output path
+OutputPath = os.path.join(BasePathForLLVM, OutputScriptPath, "Output")
+
+# Check if the Output folder exists, create it if it doesn't
+if not os.path.exists(OutputPath):
+    os.makedirs(OutputPath)
+
+# Now OutputPath is ready for use
+print("InputPath:{}".format(BasePathForLLVM))
+print(f"OutputPath: {OutputPath}")
+# exit()
 
 # ************ Don't change unless u required 
 # Define the values dictionary
-values = {
-    # "10": 10,
+inputValues = {
+    "10": 10,
     # "100": 100,
     # "1K": 1000,
     # "10K": 10000,
@@ -27,51 +47,38 @@ values = {
     # "30K": 30000,
     # "40K": 40000,
     # "50K": 50000,
-    "100K": 100000,
-    # "1M": 1000000
+    # "100K": 100000,
+    #  "1M": 1000000,
+    # "10M": 10000000,
+    # "20M": 20000000,
+    # "30M": 30000000,
+    # "40M": 40000000,
+    # "50M": 50000000,
+    # "100M": 100000000,
+    # "1B": 1000000000
 }
-NoOfIterations = 1
 
 commands_base = [
     # "./dsp1 lowPassFull.py -emit=mlir-affine",
-    f"./dsp1 {input_file_path} -emit=mlir-affine",
-    # "clang-17 file.ll -o fileexe -lm",
-    # "./fileexe"
+    # f"./dsp1 {input_file_path} -emit=mlir-affine",
+    f"{BasePathForLLVM}build/bin/dsp1 {input_file_path} -emit=mlir-affine",
+    f"{BasePathForLLVM}build/bin/dsp1 {input_file_path} -emit=llvm",
+
 ]
 
 # Define the cases
-# cases = [
-#     {"affineOpt": False, "canonOpt": False, "suffix": "file2.ll"},
-#     {"affineOpt": True, "canonOpt": False, "suffix": "file3.ll"},
-#     {"affineOpt": True, "canonOpt": True, "suffix": "file4.ll"},
-# ]
-
 cases = [
-    {"affineOpt": False, "canonOpt": False, "suffix": "fileNoOpt.mlir"},
-    {"affineOpt": True, "canonOpt": False, "suffix": "fileAffineOpt.mlir"},
-    {"affineOpt": True, "canonOpt": True, "suffix": "fileAffineCanonOpt.mlir"},
+    {"affineOpt": True, "canonOpt": True, "suffix": "fileAffineCanonOpt.mlir", "suffixll": "fileAffineCanonOpt.ll"},
+    {"affineOpt": True, "canonOpt": False, "suffix": "fileAffineOpt.mlir","suffixll": "fileAffineOpt.ll"},
+    {"affineOpt": False, "canonOpt": False, "suffix": "fileNoOpt.mlir" ,"suffixll": "fileNoOpt.ll"},
 ]
-
-
-
-# Modify the base command based on flags
-# if enableAffineOpt and enableCanonOpt:
-#     commands_base[0] += " -affineOpt -canonOpt 2> file4.ll"
-#     commands_base[1] = "clang-17 file4.ll -o fileexe -lm"
-# elif enableAffineOpt:
-#     commands_base[0] += " -affineOpt 2> file3.ll"
-#     commands_base[1] = "clang-17 file3.ll -o fileexe -lm"
-# else:
-#     commands_base[0] += " 2> file2.ll"
-#     commands_base[1] = "clang-17 file2.ll -o fileexe -lm"
-
 
 
 # Read the input file
 with open(input_file_path, "r") as file:
     lines = file.readlines()
    
-for key, value in values.items():
+for key, value in inputValues.items():
     # Update the specific line in the file
     # print("Updating for {}".format(value))
     print("\n")
@@ -81,8 +88,8 @@ for key, value in values.items():
             if line.strip().startswith("var input = getRangeOfVector("):
             # if line.strip().startswith("var N = "):
                 # Replace the second parameter with the current value
-                updated_line = f"    var input = getRangeOfVector(0, {value}, 1);\n"
-                # updated_line = f"    var N = {value} ;\n"
+                updated_line = f"    var input = getRangeOfVector(0, {value}, 0.000125);\n"
+                # updated_line = f"    var N = {value + 1} ;\n"
                 file.write(updated_line)
             else:
                 file.write(line)
@@ -91,14 +98,20 @@ for key, value in values.items():
     # Iterate through the cases and run the commands
     for case in cases:
         command_mlir = commands_base[0]
+        command_llvm = commands_base[1]
         if case["affineOpt"]:
             command_mlir += " -affineOpt"
+            command_llvm += " -affineOpt"
         if case["canonOpt"]:
             command_mlir += " -canonOpt"
-        command_mlir += f" 2> {case['suffix']}"
+            command_llvm += " -canonOpt"
+        #command_mlir += f" 2> {case['suffix']}"
+        command_mlir += f" 2> {OutputPath}/{case['suffix']}" #OutputPath
+        command_llvm += f" 2> {OutputPath}/{case['suffixll']}"
 
         commands = [
             command_mlir,
+            command_llvm,
             # f"clang-17 {case['suffix']} -o fileexe -lm",
         ] 
         print(case,end="\n")
