67ed5688a989bd6b2cd76f30151ce040cd10f153
AtharvaKhedkar <atharva.khedkar123@gmail.com>
2024-10-16 11:20:24 -0700

    add echo,hearing_aid and vibrationanalysis apps

---
+++ mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/echocancelling.py
@@ -0,0 +1,26 @@
+def main() {
+  var fs = 8000;
+  # var step = 1/8000; 
+  # print(step);
+  var t = getRangeOfVector(0,100, 0.000125);
+  var f_sig = 500;
+  var pi = 3.14159265359;
+  var getMultiplier = 2 * pi * f_sig;
+  # print(getMultiplier);
+  var getSinDuration = gain(t, getMultiplier);
+  # print(getSinDuration);
+  var clean_sig = sin(getSinDuration );
+
+  #define a noise signal with freq = 3000
+  var noise = delay(clean_sig, 2);
+  # var noise1 = gain(noise, 0.5);
+
+  var noisy_sig = clean_sig + noise;
+  # print(noisy_sig);
+  # print(clean_sig);
+  var mu = 0.01;
+  var filterSize = 32;
+  var y = lmsFilterResponse(noisy_sig, clean_sig, mu, filterSize);
+  print(y);
+
+}
